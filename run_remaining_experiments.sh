02df1ca07a2d9ba322d72866ae37ea5022ee63c1
ThanatosJun <thanatosjun@gmail.com>
2026-03-28 16:24:45 +0800

    刪除過時的實驗腳本，更新實驗日誌，並新增實驗報告文檔

--- run_remaining_experiments.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/thanatos/data_science/Kaggle-BoyGirl-TeamBD
-python main_train.py --config configs/exp1_p99_method2_paper_range.yaml
-python main_train.py --config configs/exp1_p99_method3_grouped_mean.yaml
